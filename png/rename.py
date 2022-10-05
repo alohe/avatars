cec64db5caf14ded680ebe7a253a0e7d07950d3c
Alohe <alemalohe@gmail.com>
2022-10-05 18:29:45 +0300

    Delete png directory

--- png/rename.py
+++
@@ -1,9 +0,0 @@
-# rename .png files with a random 4digit string
-import os
-import random
-
-for file in os.listdir():
-    print(file)
-    """ if file.endswith(".png"):
-        os.rename(file, str(random.randint(1000,9999))+".png")
-    """
